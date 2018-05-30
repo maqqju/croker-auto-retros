6744bdffc0d06abf4b7c44b7775dbfb7b0d50c05
Matthew Croker <matthew.croker@ixaris.com>
2018-05-30 15:31:49 +0200

    Now able to take multiple config files

--- index.js
+++ index.js
@@ -16,72 +16,78 @@ var ARGUMENTS = ((args) => {
 								}
 							}));
 
-fs.readFile("croker-retro-config.json","utf8",(err,data) => {
-	if (err) {
-		return console.log(err);
-	}
+/**
+ * [saveSlides description]
+ * @param  {[type]} slideConfigurations [description]
+ * @return {[type]}                     [description]
+ */
+function saveSlides(slideConfigurations, folder) {
+	// get retro-Reveal.js template
+	fs.readFile("slide.tmpl","utf8", (templateError, templateSource) => {
+		var slideTemplate = handlebars.compile(templateSource);
+		var slides = slideConfigurations ? new handlebars.SafeString(slideConfigurations.map((slide) => slideTemplate({slideImage : slide.imageName, comment : slide.comment || "", title : slide.title || ""})).join("\n")) : "";
 
-	var configFile = JSON.parse(data);
+		fs.readFile("retro.tmpl","utf8", (err, src) => {
+			// process through handlebars for customization
+			var template = handlebars.compile(src);
+			var retroData = {
+				retroTitle : ARGUMENTS("title") || "Retrospective", 
+				retroSubtitle : ARGUMENTS("subtitle") || "",
+				dataSlides : slides
+			}
+			var result = template(retroData);
+			// save as html file in filder
+			fs.writeFile(folder+"/retro.html", result, function(err) {
+			    if(err) {
+			        return console.log(err);
+			    }
 
-	if (Object.keys(configFile).length === 0 || !configFile[ARGUMENTS("team")]) {
-		return console.error(`Can not load croker-retrospective without proper croker-retro-config.json. Please make sure you define team configuration for [${ARGUMENTS("team")}].`);
-	}
+			    console.log("The template moved!");
+			}); 
+		})
+	})
+}
 
-	var key = ARGUMENTS("team") ? ARGUMENTS("team") : Object.keys(configFile)[0];
-	var folder = ARGUMENTS("folder") ? ARGUMENTS("folder")  : ["croker-retro",key,Date.now()].join("-");
+var CONFIGURATIONS = ARGUMENTS("config") ? ARGUMENTS("config").split(",") : [];
+var TEAM = ARGUMENTS("team") ? ARGUMENTS("team") : Object.keys(configFile)[0];
+var FOLDER = ARGUMENTS("folder") ? ARGUMENTS("folder")  : ["croker-retro",TEAM,Date.now()].join("-");
+// create folder
+fs.mkdirSync(FOLDER);
 
-	// create folder
-	fs.mkdirSync(folder);
+var allSlides = CONFIGURATIONS.map((fileName) => JSON.parse(fs.readFileSync(fileName,"utf8")))
+							  .filter((jsonConfig) => jsonConfig[TEAM])
+							  .map((jsonConfig) => jsonConfig[TEAM])
+							  .map((jsonConfig) => jsonConfig.slides)
+							  .reduce((allSlides, slideConfig) => { return [].concat(allSlides, slideConfig)}, []);
 
-	if (configFile[key].slides) {
-		configFile[key].slides.map((slide) => {
-			return (async () => {
-				const browser = await puppeteer.launch();
-				const page = await browser.newPage();
-				await page.setViewport({ width: 1920, height: 1080});
-				await page.goto(slide.url);
+allSlides.filter((slide) => slide.url)
+		 .map((slide) => {
+				return (async () => {
+					const browser = await puppeteer.launch();
+					const page = await browser.newPage();
+					await page.setViewport({ width: 1920, height: 1080});
+					await page.goto(slide.url);
 
-				if (slide.authentication) {
-					//Assuming that the entry is in base64 encoding (used in basic authentication)
-					//the format should be username:password once decoded
-					var authentication = Buffer.from(slide.authentication.basic,"base64").toString().split(":");
-					await page.type(slide.authentication.username, authentication[0]);
-					await page.type(slide.authentication.password, authentication[1]);
-					await page.click(slide.authentication.submit);
-				}
+					if (slide.authentication) {
+						//Assuming that the entry is in base64 encoding (used in basic authentication)
+						//the format should be username:password once decoded
+						var authentication = Buffer.from(slide.authentication.basic,"base64").toString().split(":");
+						await page.type(slide.authentication.username, authentication[0]);
+						await page.type(slide.authentication.password, authentication[1]);
+						await page.click(slide.authentication.submit);
+					}
 
-				if (slide.delay) {
-					await page.waitFor(slide.delay);
-				}
+					if (slide.delay) {
+						await page.waitFor(slide.delay);
+					}
 
 
-				await page.screenshot({path : folder+"/"+slide.imageName});
-				console.log(`[${slide.imageName}] saved!`);
-				await browser.close();
-			})
+					await page.screenshot({path : FOLDER+"/"+slide.imageName});
+					console.log(`[${slide.imageName}] saved!`);
+					await browser.close();
+				})
 		}).forEach((saveImage) => {
 			saveImage();
 		});
-	}
 
-	// get retro-Reveal.js template
-	fs.readFile("retro.tmpl","utf8", (err, src) => {
-		// process through handlebars for customization
-		var template = handlebars.compile(src);
-		var retroData = {
-			retroTitle : ARGUMENTS("title") || "Retrospective Title", 
-			retroSubtitle : ARGUMENTS("subtitle") || "",
-			dataSlides : configFile[key].slides ? new handlebars.SafeString(configFile[key].slides.map((slide) => `<section data-background-image=\"${slide.imageName}\"></section>`).join("\n")) : ""
-		}
-		var result = template(retroData);
-		// save as html file in filder
-		fs.writeFile(folder+"/retro.html", result, function(err) {
-		    if(err) {
-		        return console.log(err);
-		    }
-
-		    console.log("The template moved!");
-		}); 
-	})
-	
-});
+saveSlides(allSlides, FOLDER);
